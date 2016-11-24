5ccd9a3c83e636ef4e231d8af611e43b73ce73ad
Raphael Oliveira Neves <raphaeloneves@outlook.com>
2016-11-24 13:49:58 -0200

    Remoção dos dao específicos

--- br/com/raphaelneves/dao/CarroDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.raphaelneves.dao;
-
-import br.com.raphaelneves.pojo.Carro;
-
-public class CarroDao extends DaoGenerico<Carro>{
-
-}
--- br/com/raphaelneves/dao/PessoaDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.raphaelneves.dao;
-
-import br.com.raphaelneves.pojo.Pessoa;
-
-public class PessoaDao extends DaoGenerico<Pessoa>{
-
-}
